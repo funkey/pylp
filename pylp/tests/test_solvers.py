d46682a33c5855af9f554fb76f1759282c2e4d62
Jan Funke <funkej@janelia.hhmi.org>
2020-12-23 16:26:31 -0500

    Add simple solver API test

---
+++ pylp/tests/test_solvers.py
@@ -0,0 +1,45 @@
+import pylp
+import unittest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TestSolvers(unittest.TestCase):
+
+    def simple_solver_test(self, preference):
+
+        num_vars = 10
+        special_var = 5
+
+        solver = pylp.LinearSolver(
+            num_vars,
+            pylp.VariableType.Binary,
+            {
+                special_var: pylp.VariableType.Continuous
+            },
+            preference)
+
+        objective = pylp.LinearObjective()
+        for i in range(num_vars):
+            objective.set_coefficient(i, 1.0)
+        objective.set_coefficient(special_var, 0.5)
+
+        constraint = pylp.LinearConstraint()
+        for i in range(num_vars):
+            constraint.set_coefficient(i, 1.0)
+        constraint.set_relation(pylp.Relation.Equal)
+        constraint.set_value(1.0)
+
+        solver.set_objective(objective)
+        solver.add_constraint(constraint)
+
+        solution, msg = solver.solve()
+
+        self.assertEqual(solution[5], 1)
+
+    def test_any(self):
+        self.simple_solver_test(pylp.Preference.Any)
+
+    def test_gurobi(self):
+        self.simple_solver_test(pylp.Preference.Gurobi)
